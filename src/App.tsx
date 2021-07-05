87de5e997ed054e2f628b45ff7e26657c8bd3fd0
Luka <luka@pop-os.localdomain>
2021-07-05 14:15:26 +0200

    react fontawesome integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import './stylesheets/scss/global.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
+  const element = <FontAwesomeIcon icon={faCoffee} />
+
   return (
     <div className="App">
       jeba vam isus mater
+      {element}
     </div>
   );
 }
