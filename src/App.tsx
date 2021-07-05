c6be20f1ea761fbcc89bec3f81cae826280511ac
Luka <luka@pop-os.localdomain>
2021-07-05 20:57:11 +0200

    basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import './stylesheets/scss/global.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import Sidebar from './app/components/Sidebar';
+import NewsFeed from './app/components/NewsFeed';
+import Widgets from './app/components/Widgets';
 
-function App() {
-  const element = <FontAwesomeIcon icon={faCoffee} />
-
+const App = () => {
   return (
     <div className="App">
-      test text
-      {element}
-      {console.log(process.env.REACT_APP_FIREBASE_AUTH_DOMAIN)}
+      <main className="layout container">
+        <Sidebar />
+        <NewsFeed />
+        <Widgets />
+      </main>
     </div>
   );
 }
