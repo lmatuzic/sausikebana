2ab19a54e13eeed68802bad553c342958d075102
lmatuzic <luka.matuzic1@gmail.com>
2023-08-05 19:32:26 +0200

    added some shadcn components and dark theme

---
+++ components/layouts/Navigation.tsx
@@ -0,0 +1,17 @@
+export default function Navigation() {
+	return (
+		<nav className='container mx-auto'>
+			<ul>
+				<li>
+					<a
+						href='#'
+						className='text-sm font-medium text-white bg-gray-900 rounded-md '
+						aria-current='page'
+					>
+						Dashboard
+					</a>
+				</li>
+			</ul>
+		</nav>
+	);
+}
